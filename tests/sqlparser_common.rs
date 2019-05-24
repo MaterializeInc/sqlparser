4ffcab4c517fc05140f3b6c6d10d75ef2150f55c
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-05-24 14:04:42 -0400

    Support nested expressions in BETWEEN
    `BETWEEN <thing> AND <thing>` allows <thing> to be any expr that doesn't
    contain boolean operators. (Allowing boolean operators would wreak
    havoc, because of the repurposing of AND as both a boolean operation
    and part of the syntax of BETWEEN.)

--- tests/sqlparser_common.rs
+++ tests/sqlparser_common.rs
@@ -451,6 +451,33 @@ fn parse_between() {
 }
 
 #[test]
+fn parse_between_with_expr() {
+    use self::ASTNode::*;
+    use self::SQLOperator::*;
+    let sql = "SELECT * FROM t WHERE 1 BETWEEN 1 + 2 AND 3 + 4 IS NULL";
+    let select = verified_only_select(sql);
+    let low = SQLBinaryExpr {
+        left: Box::new(ASTNode::SQLValue(Value::Long(1))),
+        op: Plus,
+        right: Box::new(ASTNode::SQLValue(Value::Long(2))),
+    };
+    let high = SQLBinaryExpr {
+        left: Box::new(ASTNode::SQLValue(Value::Long(3))),
+        op: Plus,
+        right: Box::new(ASTNode::SQLValue(Value::Long(4))),
+    };
+    assert_eq!(
+        ASTNode::SQLIsNull(Box::new(ASTNode::SQLBetween {
+            expr: Box::new(ASTNode::SQLValue(Value::Long(1))),
+            low: Box::new(low),
+            high: Box::new(high),
+            negated: false,
+        })),
+        select.selection.unwrap()
+    );
+}
+
+#[test]
 fn parse_select_order_by() {
     fn chk(sql: &str) {
         let select = verified_query(sql);
